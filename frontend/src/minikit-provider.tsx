8f1a4497e750c0f2ce9a45d84a77cf85150c8e1c
MATIAS TORRES <matiiiastorres@gmail.com>
2024-12-09 20:15:00 -0300

    mati

--- frontend/src/minikit-provider.tsx
+++ frontend/src/minikit-provider.tsx
@@ -1,12 +1,12 @@
-import { MiniKit } from "@worldcoin/minikit-js";
-import { ReactNode, useEffect } from "react";
+import { MiniKit } from '@worldcoin/minikit-js';
+import { ReactNode, useEffect } from 'react';
 
 export default function MiniKitProvider({ children }: { children: ReactNode }) {
   useEffect(() => {
     MiniKit.install();
   }, []);
 
-  console.log("Is MiniKit installed correctly? ", MiniKit.isInstalled());
+  console.log('Is MiniKit installed correctly? matias ', MiniKit.isInstalled());
 
   return <>{children}</>;
 }
